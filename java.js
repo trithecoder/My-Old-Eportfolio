53263d0afd95ee9b2b1360a64971ce651d441f91
trithecoder <triyelnewry@gmail.com>
2023-09-29 20:37:27 -0400

    Update/After JS

--- java.js
+++ java.js
@@ -1,5 +1,6 @@
 let bluecolor = document.getElementById('headlinks__1');
 let wavehand = document.getElementById('body-tag-h1');
+let paratags = document.getElementById('both-para-tags');
 
 function turnBlue() {
     bluecolor.style.color = '#d05025'
@@ -16,6 +17,20 @@ function resethand() {
     wavehand.style.color = ''
     wavehand.style.textShadow = ''
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el))
 
 wavehand.addEventListener('mouseover', waveup)
 wavehand.addEventListener('mouseout', resethand)
