d2ea1e1d1652a0e8f0988ddf317c61df4a10d57f
trithecoder <triyelnewry@gmail.com>
2023-10-11 07:20:53 -0400

    Update/JS Update

--- java.js
+++ java.js
@@ -1,12 +1,12 @@
-let bluecolor = document.getElementById('headlinks__1');
+const headerlinks = document.getElementById('headlinks__1');
 let wavehand = document.getElementById('body-tag-h1');
 let paratags = document.getElementById('both-para-tags');
-
-function turnBlue() {
-    bluecolor.style.color = '#d05025'
-};
-
-bluecolor.addEventListener('click', turnBlue)
+let backchange = document.getElementById('body');
+let buttontoggle = document.getElementById('togglebut');
+let buttontogglereset = document.getElementById('togglebut1')
+let projectcolor = document.getElementById('');
+let mainheader = document.getElementById('headerli')
+let mainbodd = document.getElementById('main-bodyid');
 
 function waveup() {
    wavehand.style.color = 'blue'
@@ -16,6 +16,23 @@ function waveup() {
 function resethand() {
     wavehand.style.color = ''
     wavehand.style.textShadow = ''
+}
+
+function changebgcolor() {
+    backchange.style.backgroundColor = 'black'
+    backchange.style.transition = '1s'
+    mainbodd.style.backgroundColor = ''
+    mainbodd.style.color = ''
+    mainheader.style.backgroundColor = ''
+    headerlinks.style.color = ''
+}
+function resetbgcolor () {
+    backchange.style.backgroundColor = 'white'
+    backchange.style.transition = '1s'
+    mainbodd.style.backgroundColor = 'white'
+    mainbodd.style.color = 'black'
+    mainheader.style.backgroundColor = 'white'
+    headerlinks.style.color = 'black'
 }
 
 const observer = new IntersectionObserver((entries) => {
@@ -32,5 +49,25 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el))
 
+const lightbserver = new IntersectionObserver((switchs) => {
+    switchs.forEach((switchs) => {
+        console.log(switchs)
+        if (switchs.isIntersecting) {
+            switchs.target.classList.add('')
+        } else {
+            switchs.target.classList.remove('');
+        }
+    })
+})
+
+const lightObserver = document.querySelectorAll('.hidden');
+lightObserver.forEach((el) => observer.observe(el))
+
 wavehand.addEventListener('mouseover', waveup)
 wavehand.addEventListener('mouseout', resethand)
+
+// backchange.addEventListener('mouseover', changebgcolor)
+// backchange.addEventListener('mouseout', resetbgcolor)
+
+buttontoggle.addEventListener('click', changebgcolor)
+buttontogglereset.addEventListener('click', resetbgcolor)
